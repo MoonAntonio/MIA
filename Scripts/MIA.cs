d57e9d24a467cd00830e6c4a5d63b5d4d56db2c5
Moon Antonio <pinchodev91@gmail.com>
2017-04-03 20:01:57 +0200

    Preparando escena

---
+++ Scripts/MIA.cs
@@ -0,0 +1,23 @@
+﻿//                                  ┌∩┐(◣_◢)┌∩┐
+//																				\\
+// MIA.cs (28/03/2017)															\\
+// Autor: Antonio Mateo (Moon Antonio) 									        \\
+// Descripcion:		Controlador AI del enemigo									\\
+// Fecha Mod:		02/03/2017													\\
+// Ultima Mod:		Version inicial												\\
+//******************************************************************************\\
+
+#region Librerias
+using UnityEngine;
+#endregion
+
+namespace Pendulum.Controller
+{
+	/// <summary>
+	/// <para>Controlador AI del enemigo</para>
+	/// </summary>
+	[AddComponentMenu("Pendulum/Controller/MIA")]
+	public class MIA : MonoBehaviour
+	{
+	}
+}
